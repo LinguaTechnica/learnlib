676546dcc950d74340517a69160aadb13c7b13df
Protosac <protosacq@gmail.com>
2019-08-04 14:41:09 -0700

    Initialize.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='learnlib',
+      version='1.0',
+      description='For python beginners to learn python basics',
+      url='http://github.com/divethree/learnlib',
+      author='K Hudson',
+      author_email='dev@divethree.com',
+      license='MIT',
+      packages=['learnlib'],
+      zip_safe=False)
